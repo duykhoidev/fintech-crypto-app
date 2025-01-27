0c4f0d7eb63cfeb45e79570f80806cabac3b8dd0
Khoi Nguyen <nguyenvuduykhoi2001@gmail.com>
2025-01-27 20:42:25 +0700

    Update new supabase credentials

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,10 +2,11 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
-const supabaseUrl = "https://tfsfbwkzzskqzvvrayev.supabase.co";
+// New Credentials for supabase 
+const supabaseUrl = "https://dipwxhxnikpljhncxvlj.supabase.co";
 
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmc2Zid2t6enNrcXp2dnJheWV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg1NzI1NzksImV4cCI6MjA0NDE0ODU3OX0.dNOBTRf1qkTFVaA0U14aoO2vtrF5MkbaorSGYozau8g";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpcHd4aHhuaWtwbGpobmN4dmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NzM3OTAsImV4cCI6MjA1MzU0OTc5MH0.se_f3nx3BiTjxD06OKqCxD4r7hKWbDhpF8_zWJkybSc";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
